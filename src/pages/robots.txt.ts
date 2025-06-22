17942728899dabfdc4409d895fb2e1e67b5af739
Aditya Kajla <akajla09@users.noreply.github.com>
2025-06-21 22:53:49 -0700

    Migrate to Astro (#2)

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from 'astro';
+
+const getRobotsTxt = (sitemapURL: URL) => `User-Agent: *
+
+Sitemap: ${sitemapURL.href}
+`;
+
+export const GET: APIRoute = ({ site }) => {
+    const sitemapURL = new URL('sitemap-index.xml', site);
+    return new Response(getRobotsTxt(sitemapURL));
+};
